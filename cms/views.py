8736d6a2fad33759a63ddd216d3aee70b7cf988f
margaft <marga_rita_ft@hotmail.com>
2018-04-17 19:53:07 +0200

    Entrega ejercicio 15.5

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+
+def buscar(peticion,identificador):
+	try:
+		pagina = Pages.objects.get(name = identificador)
+		respuesta = pagina.page
+	except Pages.DoesNotExist:
+		respuesta = "No hay datos en la base de datos"
+	return HttpResponse(respuesta)
+
+def informacion(peticion):
+	respuesta = "Lista de páginas:  <br/>"
+	lista = Pages.objects.all()
+	for pag in lista:
+		respuesta += "</ol>" + str(pag.id)+ '. ' + str(pag.page) + ' es la página de '  + pag.name  + "<br/>"
+	respuesta += "</ol>"
+	return HttpResponse(respuesta)
